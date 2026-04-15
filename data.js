3c4fcbc334e92ea87b2eb61c8afbb5030adbfc47
TANVI-GHORPADE <tanvighorpade2111@gmail.com>
2026-04-15 16:16:44 +0530

    first change

--- data.js
+++ data.js
@@ -12,10 +12,10 @@ const portfolioData = {
   // ----------------------------------------------------------
   // 🧑 PERSONAL INFO
   // ----------------------------------------------------------
-  name: "Chinmay Ingle",
-  firstName: "Chinmay",   // Used in the hero greeting
-  lastName: "Ingle",      // Displayed large in the hero section
-  tagline: "Engineering Student & Aspiring Developer",
+  name: "Tanvi Ghorpade",
+  firstName: "Tanvi",   // Used in the hero greeting
+  lastName: "Ghorpade",      // Displayed large in the hero section
+  tagline: "AIML engineering student",
   resumeLink: "#",        // Link to your resume (Google Drive, etc.)
 
   // ----------------------------------------------------------
